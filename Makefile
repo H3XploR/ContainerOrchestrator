d8b5c61095d3d0553e67bf3e6209f37e79e6da9c
Ubuntu <yantoine@yantoine.241yiue3tjfe5icl4uvigpr55e.parx.internal.cloudapp.net>
2025-07-08 01:17:29 +0000

    oui

--- Makefile
+++ Makefile
@@ -4,12 +4,13 @@ all:
 	docker-compose -f srcs/docker-compose.yml up --build -d
 
 down:
-	docker-compose -f srcs/docker-compose.yml down
+	docker-compose -f srcs/docker-compose.yml down 
 
 re: down all
 
-prune:
+prune: down
 	docker system prune -a --volumes
+	docker volume rm srcs_db srcs_wp
 	rm -rf /home/yantoine/data/db
 	rm -rf /home/yantoine/data/wp
 
--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-mysql_install_db --user=mysql --ldata=/var/lib/mysql
-mysqld --user=mysql --bootstrap << EOF
+
+# Initialisation de la base de données si elle n'existe pas
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+  echo "Initialisation de la base de données..."
+
+  mysqld --user=mysql --bootstrap << EOF
 FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
@@ -8,3 +12,10 @@ CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
+
+else
+  echo "Base de données déjà initialisée."
+fi
+
+exec mysqld_safe --user=mysql
+
